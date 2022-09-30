2f15f71720f66af2c2308074bb6fdff2e985ba55
Hoang Tran <jashoang2583@gmail.com>
2022-09-30 16:34:35 -0700

    commit tree 4ac407b7804767f535e0b060813a8410ccc5ca6e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+const writeCards = ["Charlie", "Samip", "Ali"];
+writeCards(
+    [
+        "Thank you, Charlie, for the wonderful birthday gift!",
+        "Thank you, Samip, for the wonderful birthday gift!",
+        "Thank you, Ali, for the wonderful birthday gift!",
+      ]);
+
